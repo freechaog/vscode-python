e16d4568862a74f4a0e15d67e5b3e0a8466d8637
freechaog <1004228507@qq.com>
2016-12-11 22:17:27 +0800

    f

---
+++ sushu.py
@@ -0,0 +1,21 @@
+'''
+    #sdf
+'''
+
+def sushu(num):
+    """得到素数"""
+    flag = False
+    for i in range(1, num):
+        for j in range(2, i+1):
+            if j < i:
+                if i%j == 0:
+                    break
+            elif j == i:
+                flag = True
+            else:
+                break
+        if flag is True:
+            yield i
+            flag = False
+L1 = [i for i in sushu(1000)]
+print(L1)
